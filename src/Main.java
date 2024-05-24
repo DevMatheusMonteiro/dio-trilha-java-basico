99d48cd3061d2390e4ac15b45fd94526d4ff8af2
DevMatheusMonteiro <carlosmathmonteiro@gmail.com>
2024-05-24 12:42:35 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import edu.matheus.primeiraSemana.BoletimEstudantil;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println("Hello World!");
+
+        BoletimEstudantil boletimEstudantil = new BoletimEstudantil();
+
+        String [] emails = {"email1", "email2", "email3", "email4", "email5"};
+
+
+        System.out.println(Arrays.toString(emails));*/
+
+    }
+}
---
+++ src/edu/matheus/primeiraSemana/BoletimEstudantil.java
@@ -0,0 +1,4 @@
+package edu.matheus.primeiraSemana;
+
+public class BoletimEstudantil {
+}
